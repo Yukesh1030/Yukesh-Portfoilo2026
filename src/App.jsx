9cf3fe5629db9fd69679d2a0eeab7f3c72395714
Yukesh1030 <yukeshyuki18@gmail.com>
2026-09-08 16:48:15 +0530

    Modified for scroldown

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Contact from './pages/Contact';
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <ScrollToTop />
       <div className="app-container">
         <Navbar />
