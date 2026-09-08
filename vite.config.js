42c2d111b116681cd875c7ed37bae17eec2ce985
Yukesh1030 <yukeshyuki18@gmail.com>
2026-09-08 16:01:28 +0530

    Fix GitHub Pages path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Yukesh-Portfolio2026/',
+  base: '/Yukesh-Portfoilo2026/',
 })
